06da2d374be000e9c1916e08cf42803a98dc0408
yelantingxue <zsdsywr2014@126.com>
2017-11-27 17:01:17 +0800

    Interview 12 solution and test.

---
+++ src/chap2/interview12/Interview12Q1.java
@@ -0,0 +1,128 @@
+package chap2.interview12;
+
+public class Interview12Q1 {
+	
+	public static boolean findPath(char[][] chr, String str) {
+		
+		int rows = chr.length;
+		
+		//chr = []
+		if(rows == 0) {
+			return false;
+		}
+		
+		int cols = chr[0].length;
+		
+		//chr = [[]]
+		if(cols == 0) {
+			return false;
+		}
+		
+		
+		
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j < cols; j++) {
+				if(chr[i][j] == str.charAt(0)) {
+					
+					if(str.length() == 1) {
+						return true;
+					}
+					
+					//Pay attention to the position of this statement.
+					//If there is more than one character equals to the first of the string, 
+					//this statement will reset the matrix visited[][].
+					boolean[][] visited = new boolean[rows][cols];
+					
+					if(hasPath(chr, str, i, j, visited, 1, rows, cols)) {
+						return true;
+					}
+				}
+				
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * One interesting and important thing is that if a hasPath() method (B) 
+	 * is finished and return to the hasPath() method (A) that invoked it, 
+	 * the boolean matirx visited[][], will return to the A's status.
+	 * @param chr: Character Matrix.
+	 * @param str: The path to be found in character matrix.
+	 * @param row: The row of the character.
+	 * @param col: The col of the character.
+	 * @param visited: The boolean matrix that record if this character is added to the path.
+	 * @param offset: The character position in the str.
+	 * @param rows: The number of rows of chr.
+	 * @param cols: The number of cols of chr.
+	 * @return
+	 */
+	private static boolean hasPath(char[][] chr, String str, 
+			int row, int col, boolean visited[][], int offset, int rows, int cols) {
+		
+		visited[row][col] = true;
+		
+		if(row > 0 && !visited[row-1][col]) {
+			
+			//visited[row-1][col] = true;
+			
+			if(chr[row-1][col] == str.charAt(offset)) {
+				
+				if(offset >= str.length()-1) {
+					return true;
+				}
+				if(hasPath(chr, str, row-1, col, visited, offset+1, rows, cols)) {
+					return true;
+				}
+			}
+			
+		}
+		
+		if(row < rows-1 && !visited[row+1][col]) {
+			
+			//visited[row+1][col] = true;
+			
+			if(chr[row+1][col] == str.charAt(offset)) {
+				
+				if(offset >= str.length()-1) {
+					return true;
+				}
+				
+				if(hasPath(chr, str, row+1, col, visited, offset+1, rows, cols)) {
+					return true;
+				}
+			}
+		}
+		
+		if(col > 0 && !visited[row][col-1]) {
+			//visited[row][col-1] = true;
+			
+			if(chr[row][col-1] == str.charAt(offset)) {
+				
+				if(offset >= str.length()-1) {
+					return true;
+				}
+				
+				if(hasPath(chr, str, row, col-1, visited, offset+1, rows, cols)) {
+					return true;
+				}
+			}
+		}
+		
+		if(col < cols-1 && !visited[row][col+1]) {
+			//visited[row][col+1] = false;
+			
+			if(chr[row][col+1] == str.charAt(offset)) {
+				
+				if(offset >= str.length()-1) {
+					return true;
+				}
+				
+				if(hasPath(chr,str, row, col+1, visited, offset+1, rows, cols)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
--- src/coding/interview/test/Chapter2Test.java
+++ src/coding/interview/test/Chapter2Test.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 
 import chap2.interview10.Interview10Q1;
 import chap2.interview11.Interview11Q1;
+import chap2.interview12.Interview12Q1;
 import chap2.interview3.Question1;
 import chap2.interview3.Question2;
 import chap2.interview5.Interview5Q1;
@@ -22,108 +23,105 @@ import chap2.interview9.Inter9Queue;
 import chap2.interview9.QueueToStack;
 import junit.framework.TestCase;
 
-public class Chapter2Test{
-	
+public class Chapter2Test {
+
 	@Test
 	public void Question1Test() {
 		Random random = new Random();
-		
+
 		int[] a = new int[100000000];
 		a = random.ints(100000000, 0, 100000000).toArray();
-		
+
 		int[] b = new int[100000000];
-		
+
 		System.arraycopy(a, 0, b, 0, 100000000);
-		
+
 		double repeated1Start = System.currentTimeMillis();
 		Question1.repeatedNumber1(a);
 		double repeated1End = System.currentTimeMillis();
 		double repeated1Time = repeated1End - repeated1Start;
-		
-		//int[] b = {2,3,1,0,4,5,8};
+
+		// int[] b = {2,3,1,0,4,5,8};
 		double repeated2Start = System.currentTimeMillis();
 		Question1.repeatedNumber2(b);
 		double repeated2End = System.currentTimeMillis();
 		double repeated2Time = repeated2End - repeated2Start;
-		
+
 		System.out.println(repeated1Time);
 		System.out.println(repeated2Time);
 	}
 
 	@Test
 	public void Questin2Test() {
-		int[] a = {2,2,5,4,3,5,7,6};
+		int[] a = { 2, 2, 5, 4, 3, 5, 7, 6 };
 		System.out.println(Question2.repeatedNumber(a));
 	}
-	
+
 	@Test
 	public void inter4Ques1Test() {
-		int[][] a = {{1,2,8,9},
-					{2,4,9,12},
-					{4,7,10,13},
-					{6,8,11,15}};
-		int[][] b = {{}};
+		int[][] a = { { 1, 2, 8, 9 }, { 2, 4, 9, 12 }, { 4, 7, 10, 13 }, { 6, 8, 11, 15 } };
+		int[][] b = { {} };
 		System.out.println(chap2.interview4.Question1.findIn2DMatrix(a, 20));
 	}
-	
+
 	@Test
 	public void inter5Q1Test() {
 		String a = " ";
 		char[] b = a.toCharArray();
 		char[] c = new char[30];
-		for(int i = 0; i < b.length; i++) {
+		for (int i = 0; i < b.length; i++) {
 			c[i] = b[i];
 		}
 		Interview5Q1.replaceBlank(c, 30);
-		
-		for(int i = 0; i < c.length; i++) {
+
+		for (int i = 0; i < c.length; i++) {
 			System.out.println(c[i]);
 		}
 	}
-	
+
 	@Test
 	public void inter6Q1Test() {
-		
+
 		Interview6Node node1 = new Interview6Node(2);
 		Interview6Node node2 = new Interview6Node(0);
 		Interview6Node node3 = new Interview6Node(1);
 		Interview6Node node4 = new Interview6Node(7);
-		
+
 		node1.next = node2;
 		node2.next = node3;
 		node3.next = node4;
-		
+
 		Interview6Q1.printListInReversedOrder(node1);
 	}
-	
+
 	@Test
 	public void inter7Q1Test() throws Exception {
-		
-		int[] preOrder = {1,2,4,7,3,5,6,8};
-		
-		int[] inOrder = {4,7,2,1,5,3,8,6};
-		
+
+		int[] preOrder = { 1, 2, 4, 7, 3, 5, 6, 8 };
+
+		int[] inOrder = { 4, 7, 2, 1, 5, 3, 8, 6 };
+
 		Interview7BinaryTree tree = Interview7Q1.reconstructBT(preOrder, inOrder);
-		
+
 		List<Interview7BinaryTree> preList = Interview7BinaryTree.preTraverse(tree);
-		
+
 		List<Interview7BinaryTree> inList = Interview7BinaryTree.inTraverse(tree);
-		
-		for(int i = 0; i < preList.size(); i++) {
+
+		for (int i = 0; i < preList.size(); i++) {
 			System.out.print(preList.get(i).value + ", ");
 		}
-		
+
 		System.out.println();
-		
-		for(int i = 0; i < inList.size(); i++) {
+
+		for (int i = 0; i < inList.size(); i++) {
 			System.out.print(inList.get(i).value + ", ");
 		}
-		
+
 	}
-	
+
 	@Test
 	public void inter8Q1Test() {
-		
+
 		Interview8BinaryTree root = new Interview8BinaryTree(1);
 		Interview8BinaryTree node2 = new Interview8BinaryTree(2);
 		Interview8BinaryTree node3 = new Interview8BinaryTree(3);
@@ -133,48 +131,48 @@ public class Chapter2Test{
 		Interview8BinaryTree node7 = new Interview8BinaryTree(7);
 		Interview8BinaryTree node8 = new Interview8BinaryTree(8);
 		Interview8BinaryTree node9 = new Interview8BinaryTree(9);
-		
+
 		/**
 		 * Test case for plain binary tree.
 		 */
-//		root.left = node2;
-//		root.right = node3;
-//		node2.left = node4;
-//		node2.right = node5;
-//		node2.parent = root;
-//		node3.left = node6;
-//		node3.right = node7;
-//		node3.parent = root;
-//		node4.parent = node2;
-//		node5.left = node8;
-//		node5.right = node9;
-//		node5.parent = node2;
-//		node6.parent = node3;
-//		node7.parent = node3;
-//		node8.parent = node5;
-//		node9.parent = node5;
-		
+		// root.left = node2;
+		// root.right = node3;
+		// node2.left = node4;
+		// node2.right = node5;
+		// node2.parent = root;
+		// node3.left = node6;
+		// node3.right = node7;
+		// node3.parent = root;
+		// node4.parent = node2;
+		// node5.left = node8;
+		// node5.right = node9;
+		// node5.parent = node2;
+		// node6.parent = node3;
+		// node7.parent = node3;
+		// node8.parent = node5;
+		// node9.parent = node5;
+
 		/**
 		 * Test case for binary trees who don't have left node.
 		 */
-//		root.right = node2;
-//		node2.right = node3;
-//		node2.parent = root;
-//		node3.right = node4;
-//		node3.parent = node2;
-//		node4.parent = node3;
-		
+		// root.right = node2;
+		// node2.right = node3;
+		// node2.parent = root;
+		// node3.right = node4;
+		// node3.parent = node2;
+		// node4.parent = node3;
+
 		Interview8BinaryTree tree = Interview8Q1.getNext(root);
-		if(tree == null) {
+		if (tree == null) {
 			System.out.println("This node is the last node in inorder traversal.");
-		}else {
+		} else {
 			System.out.println(tree.value);
 		}
 	}
-	
+
 	@Test
 	public void inter9Q1Test() {
-		
+
 		Inter9Queue<Integer> queue = new Inter9Queue<>();
 		queue.appendTail(1);
 		queue.appendTail(2);
@@ -190,12 +188,12 @@ public class Chapter2Test{
 		System.out.println(queue.deleteHead());
 		System.out.println(queue.deleteHead());
 	}
-	
+
 	@Test
 	public void queueToStackTest() {
-		
+
 		QueueToStack<Integer> stack = new QueueToStack<>();
-		
+
 		stack.push(1);
 		stack.push(2);
 		stack.push(3);
@@ -208,26 +206,60 @@ public class Chapter2Test{
 		System.out.println(stack.pop());
 		System.out.println(stack.pop());
 	}
-	
+
 	@Test
 	public void Inter10Q1Test() {
-		
+
 		double Fib1StartTime = System.currentTimeMillis();
 		System.out.println(Interview10Q1.Fibonacci1(50));
 		double Fib1EndTime = System.currentTimeMillis();
-		
+
 		double Fib2StartTime = System.currentTimeMillis();
 		System.out.println(Interview10Q1.Fibonacci2(50));
 		double Fib2EndTime = System.currentTimeMillis();
-		
+
 		System.out.println(Fib1EndTime - Fib1StartTime);
 		System.out.println(Fib2EndTime - Fib2StartTime);
 	}
-	
+
 	@Test
 	public void Inter11Q1Test() {
-		int[] a = {1,1,0,1,1};
+		int[] a = { 1, 1, 0, 1, 1 };
 		System.out.println(Interview11Q1.minRotatedArray(a));
+	}
+
+	@Test
+	public void Inter12Q1Test() {
+		char[][] chrMatrix1 = { { 'a', 'b', 't', 'g' }, { 'c', 'f', 'c', 's' }, { 'j', 'd', 'e', 'h' } };
+
+		char[][] chrMatrix2 = { { 'a', 'b', 't', 'g' } };
+
+		char[][] chrMatrix3 = { { 'a', 'b', 'b', 't' }, { 'g', 'c', 'f', 'c' }, { 'j', 'd', 'h', 'e' } };
+
+		char[][] chrMatrix4 = { {} };
+
+		char[][] chrMatrix5 = {};
+
+		char[][] chrMatrix6 = { { 'a', 'b', 'c', 'e' }, { 's', 'f', 'c', 's' }, { 'a', 'd', 'e', 'e' } };
+		
+		//ABCEHJIG
+		//SFCSLOPQ
+		//ADEEMNOE
+		//ADIDEJFM
+		//VCEIFGGS
+		char[][] chrMatrix7 = { { 'a', 'b', 'c', 'e', 'h', 'j', 'i', 'g' }, 
+								{ 's', 'f', 'c', 's', 'l', 'o', 'p', 'q' }, 
+								{ 'a', 'd', 'e', 'e', 'm', 'n', 'o', 'e' }, 
+								{ 'a', 'd', 'i', 'd', 'e', 'j', 'f', 'm' }, 
+								{ 'v', 'c', 'e', 'i', 'f', 'g', 'g', 's' }};
+		
+		char[][] chrMatrix8 = { { 'a', 'a', 'a', 'a' }, { 'a', 'a', 'a', 'a' }, { 'a', 'a', 'a', 'a' } };
+		
+		char[][] chrMatrix9 = { { 'a' } };
+
+		String str = "b";
+
+		System.out.println(Interview12Q1.findPath(chrMatrix9, str));
 	}
 
 }
