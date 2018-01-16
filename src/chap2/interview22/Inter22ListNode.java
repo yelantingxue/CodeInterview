475fc14f1cdc7663589c6b41dc1a42b0d77ec833
yelantingxue <zsdsywr2014@126.com>
2018-01-16 18:58:57 +0800

    Interview 22 solutions and tests.

---
+++ src/chap2/interview22/Inter22ListNode.java
@@ -0,0 +1,13 @@
+package chap2.interview22;
+
+public class Inter22ListNode {
+
+	public int value;
+	public Inter22ListNode next;
+	
+	public Inter22ListNode(int value) {
+		super();
+		this.value = value;
+	}
+	
+}
