227c555d9412b372895cacd52e7b00d5eac66c49
yelantingxue <zsdsywr2014@126.com>
2018-01-17 10:30:05 +0800

    Interview 23 solutions and test.

---
+++ src/chap2/interview23/Inter23ListNode.java
@@ -0,0 +1,13 @@
+package chap2.interview23;
+
+public class Inter23ListNode {
+
+	public int value;
+	public Inter23ListNode next;
+	
+	public Inter23ListNode(int value) {
+		super();
+		this.value = value;
+	}
+	
+}
