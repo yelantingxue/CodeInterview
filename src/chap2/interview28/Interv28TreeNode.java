95c4256f1a6092ce5cf0bf54287a06f63425e722
yelantingxue <zsdsywr2014@126.com>
2018-01-18 20:50:21 +0800

    Interview 28 solutions and tests.

---
+++ src/chap2/interview28/Interv28TreeNode.java
@@ -0,0 +1,14 @@
+package chap2.interview28;
+
+public class Interv28TreeNode {
+
+	public int value;
+	public Interv28TreeNode left;
+	public Interv28TreeNode right;
+	
+	public Interv28TreeNode(int value) {
+		super();
+		this.value = value;
+	}
+	
+}
